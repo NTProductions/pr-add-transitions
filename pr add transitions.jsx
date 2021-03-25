0331c9dc95ac832d9f5aedb7ff72e2ff5e81e2d5
Nate Lovell <Natelovell99@gmail.com>
2021-03-24 23:19:22 -0600

    Create pr add transitions.jsx

---
+++ pr add transitions.jsx
@@ -0,0 +1,14 @@
+﻿// add transitions in premiere
+app.enableQE();
+
+var qeSequence = qe.project.getActiveSequence(0);
+var thisTrack = qeSequence.getVideoTrackAt(0);
+var trackItem = thisTrack.getItemAt(0);
+
+var transitions = qe.project.getVideoTransitionList();
+
+for(var i = 0; i < transitions.length; i++) {
+    $.writeln(transitions[i]);
+    }
+
+trackItem.addTransition(qe.project.getVideoTransitionByName(transitions[Math.floor(Math.random() * transitions.length)]));
